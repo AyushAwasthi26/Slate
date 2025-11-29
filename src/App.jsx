79fa64c7e32a7b6021852ac24efe0b53d020710c
Ayush Awasthi <ayushawasthi382@gmail.com>
2025-11-29 18:10:51 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './index.css'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
+
 
 const App = () => {
-  return (
-    <div>Installed all dependencies</div>
-  )
+
+  const [loading , setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      authService.getCurrentUser()
+      .then((userData)=> {
+        if (userData) {
+          dispatch(login(userData));
+        }
+        else{
+          dispatch(logout());
+        }
+      })
+      .finally(()=>setLoading(false));
+  },[])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Button = ({\
+    children,
+    type= 'button',
+    bgColor = ' bg-blue-500',
+    textColor = ' text-white',
+    className = '',
+    ...props
+}) => {
+  return (
+    <button className={`px-4 py-2 rounded-xl ${bgColor} ${textColor} ${className}`}>
+        {children}</button>
+  )
+}
+
+export default Button
